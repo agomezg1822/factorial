e2ee57d134bcceb3f54fccac7cf3b7f67a2660a0
Andres gomez <andres_felipe1822@hotmail.com>
2023-03-09 07:28:21 -0500

    v1.1.0

--- factorial.js
+++ factorial.js
@@ -1,4 +1,15 @@
-let n=parseInt();
-let factorial = 1;
 
+function factorial(){
+    let n=parseInt(document.getElementById("n").value);
+    let factorial = 1;
+    let i=2;
+
+    while(i<=n){
+        factorial*=i;
+        i++;
+    }
+
+    console.log(factorial)
+    return factorial;
+}
 
